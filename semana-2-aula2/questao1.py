ca61622ab57699d4a63a3ac1823d4d8a9903d968
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2024-02-18 19:29:13 +0000

    semana-2-atv-2-final

---
+++ semana-2-aula2/questao1.py
@@ -0,0 +1,3 @@
+#Descreva o processo de criação de um ambiente virtual em Python e por que isso pode ser útil em projetos com diferentes dependências.
+
+#Criar um ambiente virtual em Python é um processo que cria um espaço isolado dentro do pc, esse programa isolado é bom para pois permite executar programas diferentes ao mesmo tempo.
---
+++ semana-2-aula2/questao2.py
@@ -0,0 +1,3 @@
+#Discuta a importância das anotações de tipo (type hints) na linguagem Python e explique como elas podem ajudar no desenvolvimento de código.
+
+#É um bom comando para deixar notas para si mesmo ou para outros que vão olhar seu codigo.
