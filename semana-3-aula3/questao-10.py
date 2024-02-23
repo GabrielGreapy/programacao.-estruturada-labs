dd22bdd084c8f4ecb3eb97af40461bb8f1d80ce8
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2024-02-23 20:29:33 +0000

    mensagem

--- semana-3-aula3/questao-10.py
+++ semana-3-aula3/questao-10.py
@@ -1,4 +1,4 @@
 #Escreva um programa que solicite ao usuário seu nome completo e imprima as iniciais de cada palavra em letras maiúsculas. Por exemplo, se o nome for "Fulano de Tal", o programa deve imprimir "F.D.T."
 nome = str(input('Coloque seu nome:'))
 spl = nome.split()
-iniciais = spl{0}[0:1]
+iniciais = spl[0][0:1]
